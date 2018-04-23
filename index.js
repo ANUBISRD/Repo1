952fa80a9fa30b665eefe36d86829467efd425a7
ANUBISRD <r.gervazov@abv.bg>
2018-04-23 21:18:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/* models/index.js */
+
+const fs = require("fs");
+
+fs.readdirSync("./")
+    .filter(file => file.indexOf("-index") >= 0) //проверката маха файловете без model в името
+    .forEach(file => {
+        require("./" + file);
+    });
+
+console.log(fs.readdirSync("./")) //readdirSync чете имената на файловете в директорията
